845d0a44d93c2bfb62d253bc0272c766178e9073
GRyFf <robert_spataru_7@yahoo.com>
2019-05-23 02:08:48 +0300

    Project and resources setup

---
+++ ProiectDesertatieChitara/Assets/ChordScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChordScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void PopulateAudioForButtons()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProiectDesertatieChitara/Assets/NoteScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour
+{
+    private bool mouseDown;
+    private AudioSource stringSource; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stringSource = transform.parent.GetComponent<AudioSource>();
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnPointerDown()
+    {
+        mouseDown = true;
+    }
+    void OnPointerUp()
+    {
+        mouseDown = false;
+    }
+}
